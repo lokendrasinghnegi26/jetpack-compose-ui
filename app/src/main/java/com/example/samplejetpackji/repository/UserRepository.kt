e7716819fda1dea66c109f6e6c3b8baecd3f1a30
thunderstriker97 <89851170+thunderstriker97@users.noreply.github.com>
2023-01-17 11:09:48 +0530

    no changes

---
+++ app/src/main/java/com/example/samplejetpackji/repository/UserRepository.kt
@@ -0,0 +1,4 @@
+package com.example.samplejetpackji.repository
+
+class UserRepository {
+}
---
+++ app/src/main/java/com/example/samplejetpackji/viewModel/UserViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.samplejetpackji.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class UserViewModel:ViewModel() {
+
+
+}
